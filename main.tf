f568049aebc79c088dcbafa63d1c76561ade596e
wangwg <wangwg2@msn.com>
2020-05-13 22:55:29 +0800

    uu

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ resource "aws_s3_bucket" "tfstate" {
   # acl           = "authenticated-read"
 
   # Never want to delete your state bucket
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
+  lifecycle {
+    prevent_destroy = true
+  }
 
   versioning {
     enabled = true
@@ -25,9 +25,9 @@ resource "aws_dynamodb_table" "tfstate_lock" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
+  lifecycle {
+    prevent_destroy = true
+  }
 
   attribute {
     name = "LockID"
